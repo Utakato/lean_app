07d518514437c70c86c0106d3e45a4217e017fc7
Utakato <copyresearch98@gmail.com>
2022-09-25 11:22:40 +0300

    Add Progress bar component

---
+++ components/ProgressBar.tsx
@@ -0,0 +1,33 @@
+import { Typography, Box, LinearProgress } from "@mui/material";
+import React from "react";
+
+interface ProgressBarProps {
+  total: number;
+  current: number;
+  prefixText?: string;
+}
+
+export const ProgressBar: React.FC<ProgressBarProps> = ({
+  total,
+  current,
+  prefixText,
+}) => {
+  const value = (current / total) * 100;
+  const prefix = prefixText ? prefixText : "";
+  const progressInfo = `${prefix}${current}/${total} Completed`;
+
+  return (
+    <div className="w-full">
+      <Typography variant="body2" className={"text-textSecondary leading-5"}>
+        {progressInfo}
+      </Typography>
+      <Box className="w-full">
+        <LinearProgress
+          variant="determinate"
+          value={value}
+          className="rounded-sm bg-progressBackground"
+        />
+      </Box>
+    </div>
+  );
+};
