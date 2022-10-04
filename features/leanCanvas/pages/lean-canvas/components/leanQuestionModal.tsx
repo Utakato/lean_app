c1537f67e1279d5122cc5c745ac1957715694949
Utakato <copyresearch98@gmail.com>
2022-10-04 09:34:39 +0300

    Add lean canvas page and a question modal

---
+++ features/leanCanvas/pages/lean-canvas/components/leanQuestionModal.tsx
@@ -0,0 +1,149 @@
+import { Typography, TextField, Modal } from "@mui/material";
+import { useRouter } from "next/router";
+import { useState, useMemo, useEffect } from "react";
+import {
+  AppWrapper,
+  Topbar,
+  QuestionData,
+  ProgressBar,
+  FormButtons,
+} from "../../../../../components";
+import { addNewIdeaField } from "../../../../../core/api/lean-canvas";
+import { leanCanvasQuestions } from "../../../../../core/constants/LeanCanvasQuestions";
+import {
+  useAppDispatch,
+  useAppSelector,
+} from "../../../../../core/redux/store";
+import { Idea, getUserIdeasAction } from "../../../../authentication";
+import { LeanCanvasCongratulationsScreen } from "../../../../leanCanvasForm/pages/lean-canvas-form/components/LeanCanvasCongratulationsScreen";
+
+enum inputType {
+  SINGLE = "single",
+  MULTILINE = "multiline",
+}
+
+interface LeanQuestionModalProps {
+  modalInfo: { open: boolean; step: number };
+  onClose: () => void;
+}
+
+const createPayload = (key: string, value: string) => {
+  return { [key]: value };
+};
+
+export const LeanQuestionModal: React.FC<LeanQuestionModalProps> = ({
+  modalInfo,
+  onClose,
+}) => {
+  const router = useRouter();
+  const dispatch = useAppDispatch();
+  const [value, setValue] = useState("");
+  const [displayCongratulationsScreen, setDisplayCongratulationsScreen] =
+    useState(false);
+
+  const { uid, user } = useAppSelector((root) => root.authentication);
+  const { open, step } = modalInfo;
+  const currentQuestion = leanCanvasQuestions[step];
+  const isMultiline = currentQuestion.inputType === inputType.MULTILINE;
+  const totalQuestionsCount = leanCanvasQuestions.length;
+  const inputIsEmpty = value.length === 0;
+  const { ideaId } = router.query;
+  const activeIdea = useMemo(
+    () => user.ideas.filter((idea) => idea.id === ideaId)[0],
+    [user.ideas, ideaId]
+  ) as Idea;
+
+  useEffect(() => {
+    if (activeIdea) {
+      if (currentQuestion.fieldName === "name") {
+        const currentAnswer = activeIdea.name;
+        setValue(currentAnswer as string);
+      }
+      if (activeIdea?.leanCanvas[currentQuestion.fieldName]) {
+        const currentAnswer = activeIdea.leanCanvas[currentQuestion.fieldName];
+        setValue(currentAnswer as string);
+      }
+    }
+  }, [activeIdea, currentQuestion]);
+
+  useEffect(() => {
+    if (uid) {
+      dispatch(getUserIdeasAction(uid));
+    }
+  }, [uid]);
+
+  const handlePrimary = async (e: any) => {
+    e.preventDefault();
+    const payload = createPayload(currentQuestion.fieldName, value);
+    const data = {
+      ideaId: ideaId as string,
+      userId: uid,
+      payload: payload,
+    };
+    console.log(data);
+    const res = await addNewIdeaField(data);
+    onClose();
+  };
+
+  const onChange = (e: any) => {
+    setValue(e.target.value);
+  };
+
+  const handleSecondary = () => {
+    // This creates some MUI trapfocus ref error if given directly to secondaryHandler on FormButtons
+    // probably not this, this happened on some other
+    onClose();
+  };
+
+  return (
+    <Modal open={open}>
+      <AppWrapper>
+        {displayCongratulationsScreen ? (
+          <LeanCanvasCongratulationsScreen />
+        ) : (
+          <>
+            <Topbar onBack={onClose} otherStyles="absolute top-0" />
+            <form
+              // id="leanCanvas"
+              className="w-full flex flex-col gap-8"
+              // onSubmit={handleSubmit((data) => console.log(data))}
+            >
+              <QuestionData
+                title={currentQuestion.title}
+                modalContent={currentQuestion.modalComponent}
+              >
+                <Typography
+                  variant="body1"
+                  className="text-textSecondary whitespace-pre-wrap"
+                >
+                  {currentQuestion.description}
+                </Typography>
+              </QuestionData>
+              <TextField
+                multiline={isMultiline}
+                rows="4"
+                fullWidth
+                autoFocus
+                placeholder={currentQuestion.title}
+                required
+                // {...register(currentQuestion.fieldName)}
+                onChange={onChange}
+                value={value}
+              />
+              <div className="flex flex-col w-full items-center gap-5">
+                <FormButtons
+                  disabledPrimary={inputIsEmpty}
+                  formId="leanCanvas"
+                  primaryText="Save"
+                  secondaryText="Cancel"
+                  primaryHandler={(e) => handlePrimary(e)}
+                  secondaryHandler={handleSecondary}
+                />
+              </div>
+            </form>
+          </>
+        )}
+      </AppWrapper>
+    </Modal>
+  );
+};
---
+++ features/leanCanvas/pages/lean-canvas/index.tsx
@@ -0,0 +1,121 @@
+import { Button, Typography } from "@mui/material";
+import { useRouter } from "next/router";
+import { useMemo, useEffect, useState } from "react";
+import { Topbar, AppWrapper, SmallCard } from "../../../../components";
+import { useAppDispatch, useAppSelector } from "../../../../core/redux/store";
+import { routes } from "../../../../core/routes/routes";
+import { Idea, getUserIdeasAction } from "../../../authentication";
+import { LeanQuestionModal } from "./components/leanQuestionModal";
+
+enum CanvasFieldsIndexes {
+  problem = 1,
+  customer = 2,
+  solution = 3,
+  uvp = 4,
+  revenue = 5,
+  channels = 6,
+  keyMetrics = 7,
+  costs = 8,
+  unfairAdvantage = 9,
+}
+
+export const LeanCavnasPage: React.FC = () => {
+  const router = useRouter();
+  const dispatch = useAppDispatch();
+  const [modalInfo, setModalInfo] = useState({ open: false, step: 0 });
+
+  const { uid, user } = useAppSelector((root) => root.authentication);
+  const { ideaId } = router.query;
+
+  const activeIdea = useMemo(
+    () => user.ideas.filter((idea) => idea.id === ideaId)[0],
+    [user.ideas, ideaId]
+  ) as Idea;
+
+  const { name, id, leanCanvas } = activeIdea;
+
+  useEffect(() => {
+    dispatch(getUserIdeasAction(uid));
+  }, []);
+
+  const handleClick = (fieldName: string) => {
+    // @ts-ignore
+    const step = CanvasFieldsIndexes[fieldName];
+    setModalInfo({ open: true, step: step });
+  };
+
+  const handleModalClose = () => {
+    setModalInfo((prevState) => {
+      return { ...prevState, open: false };
+    });
+  };
+
+  const handleBack = () => {
+    router.push(routes.ideaDashboard(ideaId as string));
+  };
+
+  return (
+    <>
+      <Topbar onBack={handleBack} />
+      <AppWrapper>
+        <div>
+          <Typography variant="h2" className="text-black font-medium">
+            UVP
+          </Typography>
+          {leanCanvas.uvp ? (
+            <Button variant="text" onClick={() => handleClick("uvp")}>
+              <Typography variant="body1" className="text-black ">
+                {leanCanvas.uvp}
+              </Typography>
+            </Button>
+          ) : (
+            <Button variant="text" onClick={() => handleClick("uvp")}>
+              You have not defined your UVP yet... Click here to add one
+            </Button>
+          )}
+
+          <Typography variant="h2" className="text-black font-medium">
+            Product
+          </Typography>
+          <div className="flex flex-col gap-5 mt-5 mb-8">
+            <SmallCard text="Problem" onClick={() => handleClick("problem")} />
+            <SmallCard
+              text="Solution"
+              onClick={() => handleClick("solution")}
+            />
+            <SmallCard
+              text="Cost structure"
+              onClick={() => handleClick("costs")}
+            />
+            <SmallCard
+              text="Key metrics"
+              onClick={() => handleClick("keyMetrics")}
+            />
+          </div>
+          <Typography variant="h2" className="text-black font-medium">
+            Market
+          </Typography>
+          <div className="flex flex-col gap-5 mt-5 mb-8">
+            <SmallCard
+              text="Customer segments"
+              onClick={() => handleClick("customerSegments")}
+            />
+            <SmallCard
+              text="Unfair advantage"
+              onClick={() => handleClick("unfairAdvantage")}
+            />
+            <SmallCard
+              text="Revenue streams"
+              onClick={() => handleClick("revenueStreans")}
+            />
+            <SmallCard
+              text="Channels"
+              onClick={() => handleClick("channels")}
+            />
+          </div>
+        </div>
+      </AppWrapper>
+      <LeanQuestionModal modalInfo={modalInfo} onClose={handleModalClose} />
+    </>
+  );
+};
